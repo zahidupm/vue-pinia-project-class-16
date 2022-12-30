64eb86a9cec1c593653ead55459dc5070f9ab931
zahidupm <zahidhasanlimon111@gmail.com>
2022-12-30 20:52:17 +0600

    pinia state

---
+++ src/stores/TodoStore.js
@@ -0,0 +1,14 @@
+import { defineStore } from 'pinia';
+
+export const useTodoStore = defineStore('TodoStore', {
+    // state
+    state: () => ({
+        todos: [
+            {id:1, title: 'Todo', completed: false},
+            {id:2, title: 'Todo1', completed: true},
+            {id:3, title: 'Todo2', completed: false},
+        ]
+    }),
+    // actions
+    // mutations
+});
---
+++ src/views/TodoPage.vue
@@ -0,0 +1,27 @@
+<template>
+  HomePage
+  <ul>
+    <li v-for="(todo, index) in todos" :key="index">{{ todo.title }}</li>
+  </ul>
+</template>
+
+<script>
+import { mapState } from "pinia";
+import { useTodoStore } from "../stores/TodoStore";
+
+export default {
+  name: "TodoPage",
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState(useTodoStore, ["todos"]),
+    name() {
+      return "";
+    },
+  },
+  methods: {},
+};
+</script>
+
+<style scoped></style>
